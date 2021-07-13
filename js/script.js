2e23661f63652160bb97c48bc9de1f5379566520
Grzegorz Bors <35344255+grzegorzbors@users.noreply.github.com>
2021-07-13 20:00:42 +0200

    main section and part of responsive nav

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector("nav");
+const headerWithArrow = document.querySelector("#h1-with-arrow");
+
+const showNav = () => {
+  navigation.classList.toggle("mobile-nav");
+  headerWithArrow.classList.toggle("h1-with-arrow-hidden");
+};
+
+hamburger.addEventListener("click", showNav);
