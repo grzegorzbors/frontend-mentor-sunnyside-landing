dd090ca0ec1ef7740a4b2cd107be46aaffdb1203
Grzegorz Bors <35344255+grzegorzbors@users.noreply.github.com>
2021-07-21 18:15:55 +0200

    finalised mobile design

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const headerWithArrow = document.querySelector("#h1-with-arrow");
 const showNav = () => {
   navigation.classList.toggle("mobile-nav");
   headerWithArrow.classList.toggle("h1-with-arrow-hidden");
+  hamburger.classList.toggle("hamburger-opaque");
 };
 
 hamburger.addEventListener("click", showNav);
